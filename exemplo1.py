600f21045cd41a2e71c55849a920f60d7afaa729
Luciano Borba <luhborbafilho@gmail.com>
2024-03-19 12:40:59 -0300

    feat: melhorando exemplo 1

--- exemplo1.py
+++ exemplo1.py
@@ -4,3 +4,17 @@ engine = create_engine('sqlite:///meubanco.db', echo=True)
 
 
 print("Conexão com SQLite criada com sucesso!")
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    idade = Column(Integer)
+
+Base.metadata.create_all(engine)
